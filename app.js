4f96bd312b522433678ac104ad51b69dd70b5d7c
saadullah123-cyber <suashraf28@gmail.com>
2021-10-27 15:49:27 +0500

    removed comment

--- app.js
+++ app.js
@@ -6,15 +6,6 @@ const bodyparser = require('body-parser');
 
 const cors = require('cors');
 
-// const corsOptions ={
-//     origin:'http://localhost:3000', 
-//     credentials:true,            //access-control-allow-credentials:true
-//     optionSuccessStatus:200
-// }
-
-// app.use(cors(corsOptions));
-
-
 app.use(bodyparser.json());
 app.use(cors());
 
